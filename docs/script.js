066706acee1a93f25c2b0823aed71c771f8e141b
KminekD23 <KminekD23@Z00T6-05>
2025-04-01 09:58:44 +0200

    cisielka

--- docs/script.js
+++ docs/script.js
@@ -90,15 +90,15 @@ function checkCollision() {
             
             // Odrazový efekt
             if (hitTopPipe) {
-                bird.velocity = Math.abs(bird.velocity) * 0.2; // Odraz dole
-                bird.y += 5; // Posun dole
+                bird.velocity = Math.abs(bird.velocity) * 0.3; // Odraz dole
+                bird.y += 3; // Posun dole
             } 
             else if (hitBottomPipe || hitGround) {
-                bird.velocity = -Math.abs(bird.velocity) * 0.5; // Odraz hore
-                bird.y -= 5; // Posun hore
+                bird.velocity = -Math.abs(bird.velocity) * 0.6; // Odraz hore
+                bird.y -= 7; // Posun hore
             }
             else if (hitCeiling) {
-                bird.velocity = 1.5; // Začne padať
+                bird.velocity = 1.0; // Začne padať
             }
         } else {
             if (score > bestScore) {
